bbe67fac29d3a230eb1398b974202c943933a5b3
1HotSauce1 <mineralwater19@gmail.com>
2021-04-20 12:35:06 +0300

    Changed configuration method

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     # TODO: Research how to store a secret key
-    app.config['SECRET_KEY'] = 'asdadad'
+    app.config.from_object('config')
+    app.config.from_pyfile('config.py')
 
     # Views / Routing
     from .views import views
